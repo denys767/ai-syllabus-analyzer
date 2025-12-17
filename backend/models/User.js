0fcfbe66b1dcd2e6bba8a0cd0864fc74d99061af
Денис Кучерук <dkucheruk@kse.org.ua>
2025-12-17 21:36:28 +0200

    adjustment to mongoose models

--- backend/models/User.js
+++ backend/models/User.js
@@ -80,16 +80,11 @@ const userSchema = new mongoose.Schema({
 userSchema.index({ role: 1 });
 
 // Pre-save middleware to hash password
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
+
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method to compare password
